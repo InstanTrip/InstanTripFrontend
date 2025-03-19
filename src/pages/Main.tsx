c2ce472aaa19ee6ce6b6168936534ca26464a36e
An Jaebeom <ajb8533296@gmail.com>
2025-03-19 21:19:56 +0900

    메인페이지 반응형 개선

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -2,24 +2,34 @@ import { Flex, Box, Text, useBreakpointValue } from "@chakra-ui/react";
 import Background from "../images/background.webp";
 
 export default function Main() {
+    const background: string = useBreakpointValue({ base: "left", md: "center" }) as string;
+
     const logoFontSize: string = useBreakpointValue({ base: "25px", md: "30px" }) as string;
+    const logoLeft: string = useBreakpointValue({ base: "25px", md: "50px" }) as string;
+    const logoTop: string = useBreakpointValue({ base: "25px", md: "35px" }) as string;
+
+    const loginFontSize: string = useBreakpointValue({ base: "15px", md: "15px" }) as string;
+    const loginRight: string = useBreakpointValue({ base: "25px", md: "50px" }) as string;
+    const loginTop: string = useBreakpointValue({ base: "30px", md: "40px" }) as string;
+
     const mainFontSize: string = useBreakpointValue({ base: "30px", md: "48px" }) as string;
     const subFontSize: string = useBreakpointValue({ base: "18px", md: "25px" }) as string;
-    const leftMargin: string = useBreakpointValue({ base: "50px", md: "100px" }) as string;
+    const leftMargin: string = useBreakpointValue({ base: "30px", md: "100px" }) as string;
+    const textBoxGap: string = useBreakpointValue({ base: "25px", md: "45px" }) as string;
 
     return (
         <Flex w="100vw" h="100vh" >
             <Box w="100vw" h="100vh"
                 backgroundImage={`url(${Background})`}
                 backgroundRepeat="no-repeat"
-                backgroundPosition="center"
+                backgroundPosition={background}
                 backgroundSize="cover"
                 overflow="hidden"
             >
                 <Box
                     pos="fixed"
-                    top="35px"
-                    left="50px"
+                    top={logoTop}
+                    left={logoLeft}
                 >
                     <Text
                         fontSize={logoFontSize}
@@ -28,13 +38,25 @@ export default function Main() {
                     </Text>
                 </Box>
 
+                <Box
+                    pos="fixed"
+                    top={loginTop}
+                    right={loginRight}
+                >
+                    <Text
+                        fontSize={loginFontSize}
+                    >
+                        로그인 | 회원가입
+                    </Text>
+                </Box>
+
                 <Flex
-                    h="91%"
+                    h="90%"
                     alignItems="center"
                 >
                     <Flex
                         direction="column"
-                        gap="50px"
+                        gap={textBoxGap}
                         ml={leftMargin}
                     >
                         <Text
